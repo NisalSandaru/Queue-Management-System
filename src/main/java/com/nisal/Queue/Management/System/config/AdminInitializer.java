61ca9a411963e267b35e4c2da4d436ba12458de7
NisalSandaru <nisalsandaru1@gmail.com>
2025-11-23 12:37:02 +0530

    default admin create

--- src/main/java/com/nisal/Queue/Management/System/config/AdminInitializer.java
+++ src/main/java/com/nisal/Queue/Management/System/config/AdminInitializer.java
@@ -24,18 +24,29 @@ public class AdminInitializer implements CommandLineRunner {
         // Check if admin already exists
         if (!userRepository.existsByRole(UserRole.ROLE_ADMIN)) {
 
-            UserEntity admin = new UserEntity();
-            admin.setFirstName("admin");
-            admin.setLastName("admin");
-            admin.setPhone(null);
-            admin.setStatus(UserStatus.ACTIVE);
-            admin.setEmail("admin@system.com");
-            admin.setPassword(passwordEncoder.encode("admin123"));
-            admin.setRole(UserRole.ROLE_ADMIN);
+            UserEntity e = userRepository.findByEmail("admin@system.com");
 
-            userRepository.save(admin);
+            if (e != null) {
+                if (e.getRole() != UserRole.ROLE_ADMIN) {
+                    e.setRole(UserRole.ROLE_ADMIN);
+                    e.setStatus(UserStatus.ACTIVE);
+                    userRepository.save(e);
+                }
+            }else {
 
-            System.out.println("Default Admin Created: admin@system.com / admin123");
+                UserEntity admin = new UserEntity();
+                admin.setFirstName("admin");
+                admin.setLastName("admin");
+                admin.setPhone(null);
+                admin.setStatus(UserStatus.ACTIVE);
+                admin.setEmail("admin@system.com");
+                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setRole(UserRole.ROLE_ADMIN);
+
+                userRepository.save(admin);
+
+                System.out.println("Default Admin Created: admin@system.com / admin123");
+            }
         } else {
             System.out.println("Admin already exists. Skipping default admin creation.");
         }
